9ece98488903d1dc402945c6dd2f533baf6a066d
Antonio Sulejman <tocycz@gmail.com>
2022-04-09 21:43:14 +0200

    update initialModel

--- src/Main.elm
+++ src/Main.elm
@@ -1,5 +1,8 @@
 module Main exposing (..)
 
+import Browser
+import Html
+
 
 type alias Model =
     { searchText : String
@@ -11,3 +14,40 @@ type Msg
     = MsgSearch
     | MsgGotResults
     | MsgInputTextField
+
+
+main : Program () Model Msg
+main =
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( initModel, Cmd.none )
+
+
+initModel : Model
+initModel =
+    { searchText = ""
+    , results = []
+    }
+
+
+view : Model -> Html.Html Msg
+view _ =
+    Html.text "Books"
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update _ model =
+    ( model, Cmd.none )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none
